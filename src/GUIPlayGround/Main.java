56020d4e8a1c4684fbb6f267af2ba95a2f25821b
VonWalther <carl.walther@sbcglobal.net>
2019-10-15 09:55:45 -0500

    Added a Image and three Image Views, All in a VBOX / pane.

--- src/GUIPlayGround/Main.java
+++ src/GUIPlayGround/Main.java
@@ -2,33 +2,41 @@ package GUIPlayGround;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.geometry.Insets;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.Pane;
 import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 public class Main extends Application {
 
     @Override
-    public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        //primaryStage.setTitle("Hello World");
-        //primaryStage.setScene(new Scene(root, 300, 275));
-        Button btOK = new Button("OK");
-        StackPane stPane = new StackPane();
-        stPane.getChildren().add(btOK);
-        Scene myScene = new Scene(stPane,250,250);
-        primaryStage.setTitle("Push My Button");
-        primaryStage.setScene(myScene);
+    public void start(Stage primaryStage) throws Exception {
+        Pane imagePane = new VBox(50);
+        imagePane.setPadding(new Insets(5, 5, 5, 5));
+        Image myClass = new Image("THE_CLASS.png");
+        imagePane.getChildren().add(new ImageView(myClass));
+
+        ImageView viewII = new ImageView(myClass);
+        viewII.setFitHeight(50);
+        viewII.setFitWidth(300);
+        imagePane.getChildren().add(viewII);
+
+        ImageView rotView = new ImageView(myClass);
+        rotView.setRotate(90);
+        imagePane.getChildren().add(rotView);
+
+        Scene classScene = new Scene(imagePane);
+        primaryStage.setTitle("Yall mugs!");
+        primaryStage.setScene(classScene);
         primaryStage.show();
 
-        /*
-        Stage secStage = new Stage();
-        secStage.setTitle("Second Screen");
-        secStage.setScene(new Scene(new Button("All NEW and IMPROVED"), 200, 200));
-        secStage.show();
-        */
     }
 
 
