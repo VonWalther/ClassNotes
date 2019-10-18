5b92eafe9522a45d0e065ea2db7f3b1314645b18
VonWalther <carl.walther@sbcglobal.net>
2019-10-18 09:52:49 -0500

    Drawing and Text

--- src/GUIPlayGround/Main.java
+++ src/GUIPlayGround/Main.java
@@ -12,30 +12,40 @@ import javafx.scene.layout.HBox;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Ellipse;
+import javafx.scene.text.Text;
+import javafx.scene.text.TextAlignment;
 import javafx.stage.Stage;
 
 public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-        Pane imagePane = new VBox(50);
-        imagePane.setPadding(new Insets(5, 5, 5, 5));
-        Image myClass = new Image("THE_CLASS.png");
-        imagePane.getChildren().add(new ImageView(myClass));
+        Pane drawPane = new Pane();
+        drawPane.setPadding(new Insets(10,10,10,10));
+        String mesg = "~~~This will be an Ellipse!~~~";
+        Text nameOfThing = new Text(mesg);
+        nameOfThing.xProperty().bind(drawPane.widthProperty().divide(3));
+        nameOfThing.yProperty().bind(drawPane.heightProperty().subtract(50));
+        nameOfThing.setTextAlignment(TextAlignment.CENTER);
+        drawPane.getChildren().add(nameOfThing);
 
-        ImageView viewII = new ImageView(myClass);
-        viewII.setFitHeight(50);
-        viewII.setFitWidth(300);
-        imagePane.getChildren().add(viewII);
+        for(int i = 0;i<16;i++){
+            Ellipse elp = new Ellipse(100,75);
+            elp.setCenterX(150);
+            elp.setCenterY(150);
+            elp.setStroke(Color.color(Math.random(),Math.random(),Math.random()));
+            elp.setFill(Color.color(.3,.3,.3));
+            elp.setRotate(i * 180 / 16);
+            drawPane.getChildren().add(elp);
+        }
 
-        ImageView rotView = new ImageView(myClass);
-        rotView.setRotate(90);
-        imagePane.getChildren().add(rotView);
-
-        Scene classScene = new Scene(imagePane);
-        primaryStage.setTitle("Yall mugs!");
-        primaryStage.setScene(classScene);
+        Scene funWithCode = new Scene(drawPane,300,300);
+        primaryStage.setTitle("ELLIPSE");
+        primaryStage.setScene(funWithCode);
         primaryStage.show();
+
 
     }
 
