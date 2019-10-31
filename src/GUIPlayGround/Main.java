e0f1abb23b886a779478e203c9f0a743c67ff220
VonWalther <carl.walther@sbcglobal.net>
2019-10-31 09:05:53 -0500

    Now with Buttons

--- src/GUIPlayGround/Main.java
+++ src/GUIPlayGround/Main.java
@@ -1,13 +1,17 @@
 package GUIPlayGround;
 
 import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
 import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.StackPane;
@@ -22,35 +26,59 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-        Pane drawPane = new Pane();
-        drawPane.setPadding(new Insets(10,10,10,10));
-        String mesg = "~~~This will be an Ellipse!~~~";
-        Text nameOfThing = new Text(mesg);
-        nameOfThing.xProperty().bind(drawPane.widthProperty().divide(3));
-        nameOfThing.yProperty().bind(drawPane.heightProperty().subtract(50));
-        nameOfThing.setTextAlignment(TextAlignment.CENTER);
-        drawPane.getChildren().add(nameOfThing);
-
-        for(int i = 0;i<16;i++){
-            Ellipse elp = new Ellipse(100,75);
-            elp.setCenterX(150);
-            elp.setCenterY(150);
-            elp.setStroke(Color.color(Math.random(),Math.random(),Math.random()));
-            elp.setFill(Color.color(.3,.3,.3));
-            elp.setRotate(i * 180 / 16);
-            drawPane.getChildren().add(elp);
+        HBox btPane = new HBox(20);
+        btPane.setAlignment(Pos.CENTER);
+        Button btOK     = new Button("OK");
+        Button btnotOk    = new Button("Not OK");
+        Button btCancel = new Button("Cancel");
+        Button btLamdba = new Button("\u039b");
+        //OK Handler
+        OKHandlerClass okHand = new OKHandlerClass();
+        btOK.setOnAction(okHand);
+        //Not Ok Handler
+        btnotOk.setOnAction(
+        new EventHandler<ActionEvent>(){
+            @Override
+            public void handle(ActionEvent event){
+                System.out.println("I'm need Help, and cheeseburger!!!!!");
+            }
         }
+                );
+        //Cancel Handler
+        CancelHandlerClass cancelHand = new CancelHandlerClass();
+        btCancel.setOnMouseReleased(cancelHand);
+        //Lamdba Handler
 
-        Scene funWithCode = new Scene(drawPane,300,300);
-        primaryStage.setTitle("ELLIPSE");
-        primaryStage.setScene(funWithCode);
+
+        btPane.getChildren().addAll(btOK,btnotOk,btCancel,btLamdba);
+        btPane.setPrefSize(300,100);
+
+        Scene nowWithButtons = new Scene(btPane);
+
+        primaryStage.setScene(nowWithButtons);
+        primaryStage.setTitle("Handle Events");
         primaryStage.show();
-
 
     }
 
 
     public static void main(String[] args) {
         launch(args);
+    }
+
+    private class OKHandlerClass implements EventHandler<ActionEvent> {
+        @Override
+        public void handle(ActionEvent event){
+            System.out.println("OK, Hello World");
+        }
+
+    }
+
+    private class CancelHandlerClass implements EventHandler<MouseEvent>{
+        @Override
+        public void handle(MouseEvent event){
+            System.out.println("Goodbye!");
+            System.exit(1);
+        }
     }
 }
